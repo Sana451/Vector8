78795d313cb633231f1ac74f7a124e1a1dd728c5
Sana451 <sana451@mail.ru>
2026-09-17 11:01:41 +0300

    Tomtom Map deploy fix

--- frontend/src/integrations/tomtom/config.ts
+++ frontend/src/integrations/tomtom/config.ts
@@ -1,4 +1,8 @@
 import { TomTomConfig } from "@tomtom-org/maps-sdk/core"
+import { setWorkerUrl } from "maplibre-gl"
+import workerUrl from "maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url"
+
+setWorkerUrl(workerUrl)
 
 TomTomConfig.instance.put({
   apiKey: import.meta.env.VITE_TOMTOM_API_KEY,
